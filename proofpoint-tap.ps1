f6749fe631a41d872ffcbaec8d44320de8f17a7f
Julio Viana <Julio_Viana@sunsuper.com.au>
2021-05-16 08:30:51 +1000

    Initial commit

---
+++ proofpoint-tap.ps1
@@ -0,0 +1,28 @@
+﻿#Author: Julio Viana
+#Created: 2021-05-16
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+$servicePrincipal = 'service-principal'
+$secret = 'api-key'
+
+$pair = "$($servicePrincipal):$($secret)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+
+Authorization = $basicAuthValue
+
+}
+
+$uri = 'https://tap-api-v2.proofpoint.com/v2/people/vap?window=90'
+
+
+$rsp = Invoke-RestMethod -Headers $headers -Uri "$uri" -Method Get
+$rsp.users.identity|format-table
+
